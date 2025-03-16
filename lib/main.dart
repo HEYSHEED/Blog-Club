bef6229b575023f69696224a6b52dc276f3db7e1
HEYSHEED <mahshidebshirazi@gamil.com>
2025-03-15 23:18:30 -0400

    STORIES - PART 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:blog_club/data.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,6 +29,11 @@ class MyApp extends StatelessWidget {
             fontWeight: FontWeight.bold,
             color: primaryTextColor,
           ),
+          bodyMedium: TextStyle(
+            fontFamily: defaultFont,
+            color: secondTextColor,
+            fontSize: 12,
+          ),
         ),
       ),
       home: const HomeScreen(),
@@ -39,6 +46,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final stories = AppDatabase.stories;
     final ThemeData themeData = Theme.of(context);
     return Scaffold(
       body: SafeArea(
@@ -52,13 +60,13 @@ class HomeScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'Hi, Jonathan!',
+                      'Hi, Mahshid!',
                       style: themeData.textTheme.titleMedium,
                     ),
                     Image.asset(
                       'assets/img/icons/notification.png',
-                      width: 24,
-                      height: 24,
+                      width: 32,
+                      height: 32,
                     ),
                   ],
                 ),
@@ -70,10 +78,118 @@ class HomeScreen extends StatelessWidget {
                   style: themeData.textTheme.titleLarge,
                 ),
               ),
+              _StoryList(stories: stories),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _StoryList extends StatelessWidget {
+  const _StoryList({super.key, required this.stories});
+
+  final List<Story> stories;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 90,
+      child: ListView.builder(
+        itemCount: stories.length,
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.fromLTRB(32, 0, 32, 0),
+        itemBuilder: (context, index) {
+          final story = stories[index];
+
+          return _Story(story: story);
+        },
+      ),
+    );
+  }
+}
+
+class _Story extends StatelessWidget {
+  const _Story({super.key, required this.story});
+  final Story story;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(4, 0, 4, 0),
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              story.isViewed ? _ProfileViewed() : _ProfileStandard(),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset(
+                  'assets/img/icons/${story.iconFileName}',
+                  width: 24,
+                  height: 24,
+                ),
+              ),
+            ],
+          ),
+          Text(story.name),
+        ],
+      ),
+    );
+  }
+
+  Container _ProfileStandard() {
+    return Container(
+      width: 68,
+      height: 68,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(22),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          colors: [Color(0xff376AED), Color(0xff49B0E2), Color(0xff9CECFB)],
+        ),
+      ),
+      child: Container(
+        margin: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        padding: const EdgeInsets.all(5),
+        child: _ProfileCheck(),
+      ),
+    );
+  }
+
+  Widget _ProfileViewed() {
+    return SizedBox(
+      width: 68,
+      height: 68,
+      child: DottedBorder(
+        borderType: BorderType.RRect,
+        strokeWidth: 2,
+        radius: const Radius.circular(24),
+        color: const Color(0xff7B8BB2),
+        dashPattern: const [5, 3],
+        padding: EdgeInsets.all(7),
+        child: Container(
+          width: 68,
+          height: 68,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(22)),
+          child: Container(child: _ProfileCheck()),
+        ),
+      ),
+    );
+  }
+
+  Widget _ProfileCheck() {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(17),
+      child: Image.asset('assets/img/stories/${story.imageFileName}'),
     );
   }
 }
