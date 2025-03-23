5408dd24a10fdb2a9aea0f8021682d28492e0348
HEYSHEED <mahshidebshirazi@gamil.com>
2025-03-23 19:57:54 -0400

    LOGIN & SIGN UP - PART 2

--- lib/auth.dart
+++ lib/auth.dart
@@ -2,9 +2,17 @@ import 'package:blog_club/gen/assets.gen.dart';
 import 'package:blog_club/gen/fonts.gen.dart';
 import 'package:flutter/material.dart';
 
-class AuthScreen extends StatelessWidget {
+class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  static const int loginTab = 0;
+  static const int signUpTab = 1;
+  int selectedTabIndex = loginTab;
   @override
   Widget build(BuildContext context) {
     final ThemeData themeData = Theme.of(context);
@@ -18,7 +26,7 @@ class AuthScreen extends StatelessWidget {
         child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.only(bottom: 16, top: 16),
+              padding: const EdgeInsets.only(bottom: 32, top: 32),
               child: Assets.img.icons.logo.svg(width: 120),
             ),
             Expanded(
@@ -37,8 +45,22 @@ class AuthScreen extends StatelessWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Text('LOGIN', style: tabTextStyle),
-                          Text('SIGN UP', style: tabTextStyle),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                selectedTabIndex = loginTab;
+                              });
+                            },
+                            child: Text('LOGIN', style: tabTextStyle),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                selectedTabIndex = signUpTab;
+                              });
+                            },
+                            child: Text('SIGN UP', style: tabTextStyle),
+                          ),
                         ],
                       ),
                     ),
@@ -53,80 +75,8 @@ class AuthScreen extends StatelessWidget {
                         ),
                         child: SingleChildScrollView(
                           child: Padding(
-                            padding: const EdgeInsets.all(32),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Welcome Back',
-                                  style: themeData.textTheme.headlineLarge,
-                                ),
-                                const SizedBox(height: 8),
-                                Text(
-                                  'Sign in with your account',
-                                  style: themeData.textTheme.titleSmall!.apply(
-                                    fontSizeFactor: 0.9,
-                                  ),
-                                ),
-                                const SizedBox(height: 16),
-                                TextField(
-                                  decoration: InputDecoration(
-                                    label: Text('Username'),
-                                  ),
-                                ),
-                                TextField(
-                                  obscureText: true,
-                                  enableSuggestions: false,
-                                  autocorrect: false,
-                                  decoration: InputDecoration(
-                                    label: Text('Password'),
-                                  ),
-                                ),
-                                SizedBox(height: 24),
-                                ElevatedButton(
-                                  onPressed: () {},
-                                  style: ButtonStyle(
-                                    minimumSize: WidgetStateProperty.all(
-                                      Size(
-                                        MediaQuery.of(context).size.width,
-                                        60,
-                                      ),
-                                    ),
-                                    shape: WidgetStateProperty.all(
-                                      RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(12),
-                                      ),
-                                    ),
-                                  ),
-                                  child: Text('LOGIN'),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text('Forget your password?'),
-                                    TextButton(
-                                      onPressed: () {},
-                                      child: Text('Reset here'),
-                                    ),
-                                  ],
-                                ),
-                                Center(child: Text('OR SIGN IN WITH')),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Assets.img.icons.google.image(
-                                      width: 36,
-                                      height: 36,
-                                    ),
-                                    const SizedBox(width: 4),
-                                    Assets.img.icons.twitter.image(
-                                      width: 36,
-                                      height: 36,
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
+                            padding: const EdgeInsets.fromLTRB(32, 48, 32, 32),
+                            child: _Login(themeData: themeData),
                           ),
                         ),
                       ),
@@ -136,6 +86,153 @@ class AuthScreen extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Login extends StatelessWidget {
+  const _Login({super.key, required this.themeData});
+
+  final ThemeData themeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Welcome Back', style: themeData.textTheme.headlineLarge),
+        const SizedBox(height: 8),
+        Text(
+          'Sign in with your account',
+          style: themeData.textTheme.titleSmall!.apply(fontSizeFactor: 0.9),
+        ),
+        const SizedBox(height: 16),
+        TextField(decoration: InputDecoration(label: Text('Username'))),
+        PasswordTextField(),
+        SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            minimumSize: WidgetStateProperty.all(
+              Size(MediaQuery.of(context).size.width, 60),
+            ),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            ),
+          ),
+          child: Text('LOGIN'),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Forget your password?'),
+            const SizedBox(width: 8),
+            TextButton(onPressed: () {}, child: const Text('Reset here')),
+          ],
+        ),
+        const SizedBox(height: 16),
+        const Center(
+          child: Text('OR SIGN IN WITH', style: TextStyle(letterSpacing: 2)),
+        ),
+        SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Assets.img.icons.google.image(width: 36, height: 36),
+            const SizedBox(width: 4),
+            Assets.img.icons.twitter.image(width: 36, height: 36),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _SignUp extends StatelessWidget {
+  const _SignUp({super.key, required this.themeData});
+
+  final ThemeData themeData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Welcome to Blog Club', style: themeData.textTheme.headlineLarge),
+        const SizedBox(height: 8),
+        Text(
+          'Please enter your information',
+          style: themeData.textTheme.titleSmall!.apply(fontSizeFactor: 0.9),
+        ),
+        TextField(
+          decoration: InputDecoration(label: Text('Enter your Full Name')),
+        ),
+        const SizedBox(height: 16),
+        TextField(decoration: InputDecoration(label: Text('Username'))),
+        PasswordTextField(),
+        SizedBox(height: 24),
+        ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            minimumSize: WidgetStateProperty.all(
+              Size(MediaQuery.of(context).size.width, 60),
+            ),
+            shape: WidgetStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            ),
+          ),
+          child: Text('Sign Up'),
+        ),
+        const SizedBox(height: 16),
+        const Center(
+          child: Text('OR SIGN UP WITH', style: TextStyle(letterSpacing: 2)),
+        ),
+        SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Assets.img.icons.google.image(width: 36, height: 36),
+            const SizedBox(width: 4),
+            Assets.img.icons.twitter.image(width: 36, height: 36),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class PasswordTextField extends StatefulWidget {
+  const PasswordTextField({super.key});
+
+  @override
+  State<PasswordTextField> createState() => _PasswordTextFieldState();
+}
+
+class _PasswordTextFieldState extends State<PasswordTextField> {
+  bool obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: obscureText,
+      enableSuggestions: false,
+      autocorrect: false,
+      decoration: InputDecoration(
+        label: Text('Password'),
+        suffix: InkWell(
+          onTap: () {
+            setState(() {
+              obscureText = !obscureText;
+            });
+          },
+          child: Text(
+            obscureText ? 'Show' : 'Hide',
+            style: TextStyle(
+              fontSize: 14,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
         ),
       ),
     );
